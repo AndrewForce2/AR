f0f6e9eb654dcbd63f5840448925f04114191fa6
AndrewForce2 <andrei3edw@gmail.com>
2021-05-24 15:38:49 +0300

    XR_The_Cube

---
+++ Assets/Resources/Spawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.XR.WSA.Input;
+public class Spawn : MonoBehaviour
+{
+    public GameObject prefabCube;
+    public Transform spawnSpot;
+
+    GestureRecognizer gest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gest = new GestureRecognizer ();
+
+        gest.Tapped += Gest_Tapped;
+
+        gest.StopCapturingGestures();
+    }
+
+    void Gest_Tapped (TappedEventArgs obj)
+    {
+        Instantiate(prefabCube, spawnSpot.position, spawnSpot.rotation);
+    }
+}
